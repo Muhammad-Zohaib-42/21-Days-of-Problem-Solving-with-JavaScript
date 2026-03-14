f411c7f42e2a56d0746aa6dbdbf82a336298057c
Zohaib <ranjha5054@gmail.com>
2026-03-14 12:55:40 +0500

    question 2 done

--- Day-01-Number-Problems/script.js
+++ Day-01-Number-Problems/script.js
@@ -4,8 +4,25 @@
 // Output: 1 2 3 4 5
 // ✨ Teaches basic loop construction.
 
+
+/*
 const n = 10
 
 for (let i = 1; i <= n; i++) {
     console.log(i)
 }
+*/
+
+// 2. Print Numbers from N to 1 without changing the loop condition of above question
+
+// Input: 5
+// Output: 5 4 3 2 1
+// ✨ Thinking creatively and manipulating logic within the loop
+
+/*
+const n = 10
+
+for (let i = 1; i <= n; i++) {
+    console.log(11 - i)
+}
+*/
