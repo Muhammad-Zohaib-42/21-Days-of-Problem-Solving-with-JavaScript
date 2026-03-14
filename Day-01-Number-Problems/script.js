66033c7503ae154753ea6a608a7188f02b130451
Zohaib <ranjha5054@gmail.com>
2026-03-14 12:49:58 +0500

    question 1 done

--- Day-01-Number-Problems/script.js
+++ Day-01-Number-Problems/script.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+// 1. Print Numbers from 1 to N
+
+// Input: 5
+// Output: 1 2 3 4 5
+// ✨ Teaches basic loop construction.
+
+const n = 10
+
+for (let i = 1; i <= n; i++) {
+    console.log(i)
+}
