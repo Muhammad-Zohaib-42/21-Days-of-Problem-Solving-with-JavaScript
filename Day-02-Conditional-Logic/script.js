a166860b63a2ec3aa09596ce1a56c7bea4a48f19
Zohaib <ranjha5054@gmail.com>
2026-03-15 14:15:07 +0500

    day 2 question 1 done

--- Day-02-Conditional-Logic/script.js
+++ Day-02-Conditional-Logic/script.js
@@ -3,3 +3,12 @@
 // Input: 10 20 5
 // Output: 20
 // ✨ Teaches nested or chained conditional logic and comparative reasoning.
+
+function findMax(num1, num2, num3) {
+    let maxNum = num1
+
+    maxNum = num2 > maxNum ? num2 > num3 ? num2 : num3 : maxNum
+    console.log(maxNum) 
+}
+
+findMax(100, 20, 50)
