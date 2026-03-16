96170a984e639c3166fb536ffa556f5162fa4979
Zohaib <ranjha5054@gmail.com>
2026-03-16 16:10:43 +0500

    day 3 question 1 done

---
+++ Day-03-Loops-And-Patterns/script.js
@@ -0,0 +1,20 @@
+// 1. Print Right-Angled Star Triangle
+
+// Input: Rows = 5
+
+// Output:
+
+// *
+// **
+// ***
+// ****
+// *****
+// ✨ Teaches nested loops and increasing sequence printing.
+
+// for (let i = 1; i <= 5; i++) {
+//     let str = ""
+//     for (let j = 1; j <= i; j++) {
+//         str += "*"
+//     }
+//     console.log(str)
+// }
