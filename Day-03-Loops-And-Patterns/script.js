40b6f73c581d0f86052c989d6894d8d00c1cf37c
Zohaib <ranjha5054@gmail.com>
2026-03-16 16:40:03 +0500

    day 3 question 3 done

--- Day-03-Loops-And-Patterns/script.js
+++ Day-03-Loops-And-Patterns/script.js
@@ -32,10 +32,32 @@
 // *
 // ✨ Reinforces decreasing nested loop structure.
 
-for (let i = 5; i >= 0; i--) {
-    let str = ""
-    for (let j = 1; j <= i; j++) {
-        str += "*"
-    }
-    console.log(str)
-}
+// for (let i = 5; i >= 0; i--) {
+//     let str = ""
+//     for (let j = 1; j <= i; j++) {
+//         str += "*"
+//     }
+//     console.log(str)
+// }
+
+// 3. Print Pyramid Pattern
+
+// Input: Rows = 5
+
+// Output:
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+// ✨ Teaches alignment using spaces and nested loops.
+
+// for (let i = 1; i <= 9; i += 2) {
+//     let str = ""
+//     for (let j = 1; j <= 9; j++) {
+//         if (j <= (9 - i) / 2 || j > ((9 - i) / 2) + i) str += " "
+//         else str += "*"
+//     }
+//     console.log(str)
+// }
