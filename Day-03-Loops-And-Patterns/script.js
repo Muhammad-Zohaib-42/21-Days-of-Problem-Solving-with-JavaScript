babace3f759ad09de9e9e6e1e3d84b4767b12e9b
Zohaib <ranjha5054@gmail.com>
2026-03-16 21:24:54 +0500

    day 3 question 8 done

--- Day-03-Loops-And-Patterns/script.js
+++ Day-03-Loops-And-Patterns/script.js
@@ -160,3 +160,28 @@
 //     console.log(str)
 //     start = start === 1 ? 0 : 1
 // }
+
+// 8. Print Hollow Inverted Pyramid
+
+// Input: Rows = 5
+
+// Output:
+
+// *********
+//  *     *
+//   *   *
+//    * *
+//     *
+// ✨ Focuses on conditionally printing edges and spaces.
+
+for (let i = 9; i >= 1; i -= 2) {
+    let str = ""
+    for (let j = 1; j <= 9; j++) {
+        let start = ((9 - i) / 2) + 1
+        let end = ((9 - i) / 2) + i
+        if (j < start || j > end) str += " "
+        else if (i === 9 || j === start || j === end) str += "*"
+        else str += " "
+    }
+    console.log(str)
+}
