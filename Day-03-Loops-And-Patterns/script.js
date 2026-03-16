b88279cedf8e2724583681557fec9cb689c95d71
Zohaib <ranjha5054@gmail.com>
2026-03-16 18:59:28 +0500

    day 3 question 6 done

--- Day-03-Loops-And-Patterns/script.js
+++ Day-03-Loops-And-Patterns/script.js
@@ -95,18 +95,43 @@
 // *****
 // ✨ Practices conditional printing within nested loops.
 
-for (let i = 1; i <= 5; i++) {
+// for (let i = 1; i <= 5; i++) {
+//     let str = ""
+//     for (let j = 1; j <= 5; j++) {
+//         if (i > 1 && i < 5) {
+//             if (j > 1 && j < 5) {
+//                 str += " "
+//             } else {
+//                 str += "*"
+//             }
+//         } else {
+//             str += "*"
+//         }
+//     }
+//     console.log(str)
+// }
+
+// 6. Print Hollow Pyramid Pattern
+
+// Input: Rows = 5
+
+// Output:
+
+//     *
+//    * *
+//   *   *
+//  *     *
+// *********
+// ✨ Combines conditionals and symmetrical logic in loops.
+
+for (let i = 1; i <= 9; i += 2) {
     let str = ""
-    for (let j = 1; j <= 5; j++) {
-        if (i > 1 && i < 5) {
-            if (j > 1 && j < 5) {
-                str += " "
-            } else {
-                str += "*"
-            }
-        } else {
-            str += "*"
-        }
+    for (let j = 1; j <= 9; j++) {
+        let start = ((9 - i) / 2) + 1
+        let end = ((9 - i) / 2) + i
+        if (j < start || j > end) str += " "
+        else if (i === 9 || j === start || j === end) str += "*"
+        else str += " "
     }
     console.log(str)
 }
