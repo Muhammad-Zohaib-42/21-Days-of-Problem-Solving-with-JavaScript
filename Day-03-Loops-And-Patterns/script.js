2860585bef8affb8daae1301ba4d821309865807
Zohaib <ranjha5054@gmail.com>
2026-03-16 16:16:27 +0500

    day 3 question 2 done

--- Day-03-Loops-And-Patterns/script.js
+++ Day-03-Loops-And-Patterns/script.js
@@ -18,3 +18,24 @@
 //     }
 //     console.log(str)
 // }
+
+// 2. Print Inverted Right-Angled Triangle
+
+// Input: Rows = 5
+
+// Output:
+
+// *****
+// ****
+// ***
+// **
+// *
+// ✨ Reinforces decreasing nested loop structure.
+
+for (let i = 5; i >= 0; i--) {
+    let str = ""
+    for (let j = 1; j <= i; j++) {
+        str += "*"
+    }
+    console.log(str)
+}
